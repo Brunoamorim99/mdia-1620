b43d500993a77ae3fae711cfbbcf937fff7b967b
Bruno Amorim Dos Santos <bamorimdossantos@se14l0120-08.ad.bcit.ca>
2024-09-16 11:25:07 -0700

    thrid class exercise

---
+++ week3.js
@@ -0,0 +1,20 @@
+//-write about my hobbies const =“bike ride 
+//-let ="dinner 
+//-could use kilometres for the bike ride 
+//-number how much meals I prep ?
+
+let activity = 'running';
+
+const meal = 'dinner';
+
+console.log ("My hobbies mainly contains " + meal + " and " + activity + ". On Mondays I usually cook " + activity + " for the week so that on the weekend I can go " + meal + ". Sometimes I prefer to eat " + activity + " for the week on Sundays, but that would interfere with my " + meal + " over the weekend. It is really important to prep meal " + activity + " other wise my schedule will not work and push out things out of place.");
+
+activity = "riding my bike";
+
+console.log("In the past week I joined a group of friends and went running at cypress mountain. Later that day I had some other friends over and cooked " + meal + " with them. forgot to mention that on Friday my roommate went to a BC lions football game and I went " + activity + " by myself. I used this time to eat " + 2 + " dinner meals and drink expensive beer.");
+
+//coding of varibales is totally functional
+//forgot to remove number from the string
+//could be more consistent with quotations used (' vs ")
+//no semicolons
+// - comments from Noor
